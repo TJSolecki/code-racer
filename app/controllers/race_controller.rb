8fd4cda61d6ff61666cd16f8e974b58bd0c680a9
Thomas Solecki <thomasjsolecki@gmail.com>
2023-06-19 19:20:53 -0700

    added css for user input feedback. just need to add the corresponding javascript part now

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -4,12 +4,10 @@ class RaceController < ApplicationController
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
-
         for n in nums:
             count[n] = 1 + count.get(n, 0)
         for n, c in count.items():
             freq[c].append(n)
-
         res = []
         for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
@@ -19,6 +17,10 @@ class RaceController < ApplicationController
 
     pattern = /(\S+\s{0,1})/
     @sections = @prompt.scan(pattern).flatten
+    lines = @prompt.split("\n")
+    @whitespace_array = lines.map { |line| line.match(/^(\s*)/)[0] }
+
+    p @whitespace_array
     puts @sections.inspect
     puts @prompt
   end
